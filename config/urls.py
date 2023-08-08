e5cde860bb718c5e85eecea196ad5dec1ddab3ce
satoshi <dev.satoshi.1016@gmail.com>
2023-08-09 08:49:08 +0900

    add: JWT認証を使ったアプリを作成

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/", include("apiv1.urls")),
+    re_path("", RedirectView.as_view(url="/")),
 ]
