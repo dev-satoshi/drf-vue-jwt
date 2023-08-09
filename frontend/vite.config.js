8424604c45e637fc9ac3751a3fa6c170c89ab8ae
satoshi <dev.satoshi.1016@gmail.com>
2023-08-09 09:43:27 +0900

    chore: 細かい設定を追加

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    // アセットを出力するディレクトリを「assets」から「static/spa」に変更
+    // index.htmlのアセットパスも変更される
+    assetsDir: 'static/spa',
   }
 })
